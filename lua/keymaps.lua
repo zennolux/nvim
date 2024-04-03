cc00418105d9adf6468d215bc20397236c692678
zennolux <zennolux@gmail.com>
2024-04-03 18:33:56 +0800

    chore: update

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,15 +1,11 @@
 local map = vim.keymap.set
 local options = { silent = true }
 
---nvim-tree
-map("n", "<C-b>", ":NvimTreeToggle<CR>", options)
-
 --lspsaga
 map("n", "gf", ":Lspsaga finder<CR>", options)
 map("n", "gd", ":Lspsaga peek_definition<CR>", options)
 map("n", "ge", ":Lspsaga diagnostic_jump_next<CR>", options)
 map("n", "K", ":Lspsaga hover_doc<CR>", options)
-
 map("n", "rn", ":Lspsaga rename<CR>", options)
 map("n", "<Leader>t", ":Lspsaga outline<CR>", options)
 
@@ -24,4 +20,5 @@ map("n", "<Leader>f", ":lua require('telescope.builtin').find_files({search_dirs
 map("n", "<Leader>s", ":lua require('telescope.builtin').live_grep({search_dirs = {require('utils').get_project_root()}})<CR>", options)
 
 --nvimtree
+map("n", "<C-b>", ":NvimTreeToggle<CR>", options)
 map("n", "<C-o>", ":lua require('nvim-tree.api').tree.change_root_to_node()<CR>", options)
