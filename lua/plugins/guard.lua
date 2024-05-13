f38ac81690b7fba1969a962fc7e3996c6aba0578
zennolux <zennolux@gmail.com>
2024-05-13 20:20:37 +0800

    chore: add formatter support for json,yaml

--- lua/plugins/guard.lua
+++ lua/plugins/guard.lua
@@ -1,25 +1,25 @@
-local ft = require('guard.filetype')
+local ft = require("guard.filetype")
 
-ft('html,css,javascript,typescript,typescriptreact'):fmt({
-	cmd = 'prettier',
-  args = { '--stdin-filepath' },
-  fname = true,
-  stdin = true,
+ft("html,css,javascript,typescript,typescriptreact,json,yaml"):fmt({
+	cmd = "prettier",
+	args = { "--stdin-filepath" },
+	fname = true,
+	stdin = true,
 })
 
-ft('rust'):fmt({
-	cmd = 'rustfmt',
-  args = { '--edition', '2021', '--emit', 'stdout' },
-  stdin = true,
+ft("rust"):fmt({
+	cmd = "rustfmt",
+	args = { "--edition", "2021", "--emit", "stdout" },
+	stdin = true,
 })
 
-ft('lua'):fmt({
- cmd = 'stylua',
-  args = { '-' },
-  stdin = true,
+ft("lua"):fmt({
+	cmd = "stylua",
+	args = { "-" },
+	stdin = true,
 })
 
-require('guard').setup({
+require("guard").setup({
 	fmt_on_save = true,
 	lsp_as_default_formatter = true,
 })
