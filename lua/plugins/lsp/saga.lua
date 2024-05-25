e57a01b2fdd56e18f5ed50c7071e129a2f401a57
zennolux <zennolux@gmail.com>
2024-05-25 23:41:49 +0800

    chore: add `def` for finder

--- lua/plugins/lsp/saga.lua
+++ lua/plugins/lsp/saga.lua
@@ -1,13 +1,12 @@
- require('lspsaga').setup({
-	 ui = {
-		 code_action = "",
-	 },
-	 finder = {
-		 default = "ref+imp",
-	 },
-	 lightbulb = {
-		 enable = true,
-		 sign = "",
-	 },
- })
-
+require("lspsaga").setup({
+	ui = {
+		code_action = "",
+	},
+	finder = {
+		default = "ref+imp+def",
+	},
+	lightbulb = {
+		enable = true,
+		sign = "",
+	},
+})
