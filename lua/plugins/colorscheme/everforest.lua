b8b76c73849614fc0e4e40087228a98cc9d1a4e7
zennolux <zennolux@gmail.com>
2024-05-28 18:25:36 +0800

    chore: use lua api to set highlights and customize disgnostic highlights

--- lua/plugins/colorscheme/everforest.lua
+++ lua/plugins/colorscheme/everforest.lua
@@ -9,15 +9,41 @@ vim.g.everforest_diagnostic_line_highlight = 0
 vim.g.everforest_diagnostic_virtual_text = "colored"
 vim.g.everforest_float_style = "dim"
 
-vim.cmd([[
-  autocmd ColorScheme * highlight NormalFloat guibg=NONE ctermbg=NONE
-  autocmd ColorScheme * highlight Float guibg=NONE ctermbg=NONE
-  autocmd ColorScheme * highlight FloatBorder guibg=NONE ctermbg=NONE
-  autocmd ColorScheme * highlight FloatShadow guibg=NONE ctermbg=NONE
-	autocmd ColorScheme * highlight SpellBad cterm=NONE gui=NONE guisp=NONE
-	autocmd ColorScheme * highlight SpellCap cterm=NONE gui=NONE guisp=NONE
-	autocmd ColorScheme * highlight SpellLocal cterm=NONE gui=NONE guisp=NONE
-	autocmd ColorScheme * highlight SpellRare cterm=NONE gui=NONE guisp=NONE
-]])
+local api = vim.api
+local float_opts = { bg = "NONE", ctermbg = "NONE" }
+local spell_opts = { ctermbg = "NONE", bg = "NONE", sp = "NONE" }
+
+api.nvim_create_autocmd("ColorScheme", {
+	pattern = "*",
+	callback = function()
+		--set float window highlight to NONE
+		api.nvim_set_hl(0, "NormalFloat", float_opts)
+		api.nvim_set_hl(0, "Float", float_opts)
+		api.nvim_set_hl(0, "FloatBorder", float_opts)
+		api.nvim_set_hl(0, "FloatShadow", float_opts)
+		--set spell highlight to NONE
+		api.nvim_set_hl(0, "SpellBad", spell_opts)
+		api.nvim_set_hl(0, "SpellCap", spell_opts)
+		api.nvim_set_hl(0, "SpellLocal", spell_opts)
+		api.nvim_set_hl(0, "SpellRare", spell_opts)
+		--set diagnostic highlight
+		api.nvim_set_hl(0, "ErrorText", {
+			ctermfg = 167,
+			fg = 15105664,
+		})
+		api.nvim_set_hl(0, "InfoText", {
+			ctermfg = 109,
+			fg = 8371123,
+		})
+		api.nvim_set_hl(0, "HintText", {
+			ctermfg = 142,
+			fg = 10993792,
+		})
+		api.nvim_set_hl(0, "WarningText", {
+			ctermfg = 214,
+			fg = 14400639,
+		})
+	end,
+})
 
 vim.cmd.colorscheme("everforest")
