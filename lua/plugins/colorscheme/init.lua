80feef56b30d8fb05254231bd6f116ab8a65fc75
zennolux <zennolux@gmail.com>
2024-06-07 09:28:11 +0800

    chore: prevent semantic_token overwrite treesitter color

--- lua/plugins/colorscheme/init.lua
+++ lua/plugins/colorscheme/init.lua
@@ -1,2 +1,5 @@
+-- Prevent LSP from overwriting treesitter color settings
+vim.highlight.priorities.semantic_tokens = 95
+-- Colorscheme in use
 local colorscheme = require("plugins.colorscheme.everforest")
 return colorscheme
